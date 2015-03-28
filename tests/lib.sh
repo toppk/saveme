2e179131204e9414e4bcf480135bcd1c4f78bcd3
root <root@static.bllue.org>
2015-03-28 14:23:12 -0400

    - new library routine for testing

---
+++ tests/lib.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+function runme()
+{
+
+    sleep=0
+    if [ "$1" == "--sleep" ]; then
+	sleep=10
+	shift
+    fi
+    id=$1
+    shift
+    comm=$*
+    echo "# STARTING: id=$id comm=[$comm]"
+    $comm > $id.out 2> $id.err
+    err=$?
+    if [ $err -ne 0 ]; then
+	echo "#  FAILURE: error $err during starting $id"
+	return 1
+    fi
+    echo "#  SUCCESS: started $id"
+    echo "#  RUNNING: id=$id comm=[bash -e ./$id.out] contents=[$( tr '\n' ';' < $id.out )]"
+    if [ $sleep -ne 0 ]; then
+	echo "#   notice: sleeping for $sleep seconds"
+	sleep $sleep
+    fi
+    bash -e ./$id.out > ${id}_RUN.out 2> ${id}_RUN.err
+    err=$?
+    if [ $err -ne 0 ]; then
+	echo "#  FAILURE: error $err during running $id"
+	return 2
+    fi
+    . <( cat $id.out  | sed -n  "/^#/s/.*'\(.*\)':'\(.*\)'$/\1=\2/p" )
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    runme "should-fail-starting" ls -d /asdf / && exit || echo "#  SUCCESS: it failed above us"
+    runme "should-fail-running" echo ls -d /asdf / && exit || echo "#  SUCCESS: it failed above us"
+    runme "should-succeed" echo ls -d / && echo "#  SUCCESS: it returned" || exit
+    key=""
+    runme "should-passfact" echo "# 'key':'value'" || exit
+    [ "$key" != "value" ] && echo "#  FAILURE: error seeing key during test" || echo "#  SUCCESS: seeing value==$key during test"
+    key2=""
+    runme --sleep "should-passfact-double" echo -e "# 'key':'value'\n# 'key2':'value2'" || exit
+    [ "$key2" != "value2" ] && echo "#  FAILURE: error seeing key2 during test" || echo "#  SUCCESS: seeing key2 during test"
+fi
--- tests/lowlevel.sh
+++ tests/lowlevel.sh
@@ -17,85 +17,34 @@ if [ $0 != "./lowlevel.sh" ]; then
     exit 2
 fi
 
+. ./lib.sh
+
 disk=$1
 
-# init
-echo \# STARTING: disk-add-part
-../scripts/disk-add-part.sh $disk > lowlevel_disk-add-part_$disk.out 2> lowlevel_disk-add-part_$disk.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: disk-add-part retval=$err
-    more lowlevel_disk-add-part_$disk.out lowlevel_disk-add-part_$disk.err | cat
-    exit 3
-fi
 
-# last chance
-echo \# WARNING: I AM ABOUT TO DESTROY /dev/$disk!!!!!
-echo \# notice: waiting for ten seconds
-sleep 10
-
-# run
-echo \# RUNNING: disk-add-part
-bash -e lowlevel_disk-add-part_$disk.out > lowlevel_disk-add-part_RUN.out 2> lowlevel_disk-add-part_RUN.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: during run of lowlevel_disk-add-part_$disk.out
-    exit 6
-fi
+runme --sleep "lowlevel_disk-add-part_$disk" ../scripts/disk-add-part.sh $disk || exit
 
 #what should work
 if ! lsblk /dev/$disk -n -o type | grep -q part; then
-    echo \# FAILURE: $disk does not have a partition type
-    exit 4
-fi
-echo \# SUCCESS: $disk has a partition type
-
-#init 
-echo \# STARTING: core-gen-key
-../scripts/core-gen-key.sh > lowlevel_core-gen-key.out 2> lowlevel_core-gen-key.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: core-gen-key retval=$err
-    more lowlevel_core-gen-key.out lowlevel_core-gen-key.err | cat
+    echo "#  FAILURE: $disk does not have a partition type"
     exit 3
 fi
-#run
-echo \# RUNNING: core-gen-key
-bash -e lowlevel_core-gen-key.out > lowlevel_core-gen-key_RUN.out 2> lowlevel_core-gen-key_RUN.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: during run of lowlevel_disk-add-part_$disk.out
-    exit 7
+echo '#  SUCCESS: $disk has a partition type"
+
+key=""
+runme "lowlevel_core-gen-key" ../scripts/core-gen-key.sh || exit
+
+#what should work
+if [ ! -f $key ]; then
+    echo "#  FAILURE: $keyf is not a file"
+    exit 4
 fi
-keyf=$( cat lowlevel_core-gen-key.out | sed -n  "/^#/s/.*'key':'\(.*\)'$/\1/p" )
-if [ ! -f $keyf ]; then
-    echo \# FAILURE: $keyf is not a file
-    exit 8
-fi
-echo \# SUCCESS: core-gen-key
+echo "#  SUCCESS: core-gen-key"
 
 part="${disk}1"
 cleankeyf=$( echo $keyf | tr '/' '+' )
 
-echo \# STARTING: part-add-luks
-../scripts/part-add-luks.sh $part $keyf  > lowlevel_part-add-luks_${part}_${cleankeyf}.out 2> lowlevel_part-add-luks_${part}_${cleankeyf}.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: part-add-luks retval=$err
-    more lowlevel_part-add-luks_${part}_${cleankeyf}.out lowlevel_part-add-luks_${part}_${cleankeyf}.err | cat
-    exit 3
-fi
-echo \# RUNNING: part-add-luks
-bash -e lowlevel_part-add-luks_${part}_${cleankeyf}.out > lowlevel_part-add-luks_${part}_${cleankeyf}_RUN.out 2> lowlevel_part-add-luks_${part}_${cleankeyf}_RUN.err
-err=$?
-if [ $err -ne 0 ]; then
-    echo \# FAILURE: during run of lowlevel_disk-add-part_$disk.out
-    exit 7
-fi
-keyf=$( cat lowlevel_core-gen-key.out | sed -n  "/^#/s/.*'key':'\(.*\)'$/\1/p" )
-if [ ! -f $keyf ]; then
-    echo \# FAILURE: $keyf is not a file
-    exit 8
-fi
-echo \# SUCCESS: core-gen-key
+cryptpart=""
+runme "lowlevel_part-add-luks_$part_$cleankeyf" ../scripts/part-add-luks.sh $part $keyf || exit
+cryptpart
 
