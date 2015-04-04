1a2f19d2d442a219b6b60286b1f3af031a3d7a1d
toppk <toppk@bllue.org>
2015-04-04 01:48:25 -0400

    -test scenario finished, need checks now

--- tests/snapshots.sh
+++ tests/snapshots.sh
@@ -33,7 +33,43 @@ if [ ! -z "$output" ] ; then
 fi
 echo "# this was supposed to fail"
 # == STEP 2 - create  ==
-launch "snapshots_create" ../tools/snapmgr create $path --noprompt || exit
+
+outdir="$path/saveme.testdir"
+if [ -d $outdir ]; then
+    echo removing directory $outdir
+    rm -rf $outdir
+fi
+
+now=$( date +%s )
+let oneweek0hr="$now - 7 * 24 * 60 * 60"
+let oneweek1hr="$oneweek0hr - 60 * 60"
+let oneweek2hr="$oneweek1hr - 60 * 60"
+let now1hr="$now - 60 * 60"
+let now2hr="$now1hr - 60 * 60"
+let now3hr="$now2hr - 60 * 60"
+
+mkdir $outdir
+echo content1 > $outdir/before.1
+echo content2 > $outdir/before.2
+echo content3 > $outdir/before.3
+../tools/snapmgr create $path --label="$( date -d @$oneweek2hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+echo content4 > $outdir/during.4
+../tools/snapmgr create $path --label="$( date -d @$oneweek1hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+echo content5 > $outdir/during.4
+echo content6 > $outdir/during.5
+echo content7 > $outdir/during.6
+rm -f $outdir/before.2
+../tools/snapmgr create $path --label="$( date -d @$oneweek0hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+rm -f $outdir/during.5
+echo content8 > $outdir/before.1
+../tools/snapmgr create $path --label="$( date -d @$now3hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+rm -f $outdir/during.6
+../tools/snapmgr create $path --label="$( date -d @$now2hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+echo content3 > $outdir/current.3
+../tools/snapmgr create $path --label="$( date -d @$now1hr '+%Y%m%d_%H:%M:%S_%z')" --noprompt
+rm -f $outdir/during.6
+rm -f $outdir/during.4
+echo content1 > $outdir/after.1
+echo content2 > $outdir/after.2
+
 echo "# Success"
-
-
